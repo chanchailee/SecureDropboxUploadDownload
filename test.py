7108f6786e479ce53a8ae8a58fdbb3914547691d
Chanchai <chanchai@uab.edu>
2018-01-30 11:09:51 -0600

    finished hash key1

--- test.py
+++ test.py
@@ -1,15 +1,29 @@
-import dropbox, hashlib
+#!/usr/bin/env python3
+
+import dropbox, hashlib, os
+from Crypto.Cipher import AES
 
 #Read input file
+f = open("input.txt","r") #opens file with name of "test.txt"
+
+input = f.read()
+print("################ read() #######################")
+print(input)
+
+#Create key1 by hash input file with sha256
+hash_object = hashlib.sha256(input.encode('utf-8'))
+#hex_dig = hash_object.hexdigest()
+key1 = hash_object.hexdigest()
+
+#print("################print hash_object #######################")
+#print(hash_object)
+
+print("################print key1 from hashlib.sha256(input) #######################")
+print(key1)
 
 
+# Create Cipher text C with AES Counter mode
 
-#Create Key by hash input file with sha256
-
-input = 'hello'
-hash_object = hashlib.sha256(b'test')
-hex_dig = hash_object.hexdigest()
-#print(hex_dig)
 
 
 
@@ -25,4 +39,4 @@ dbx = dropbox.Dropbox(token[0])
 #get dropbox user account
 
 dbx_user = dbx.users_get_current_account()
-print(dbx_user)
+#print(dbx_user)
