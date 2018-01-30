0939bfcbc00b30dc1c002b60ca875e530d6bbbce
Chanchai <chanchai@uab.edu>
2018-01-29 18:38:07 -0600

    get dropbox user from application token

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-import dropbox
+import dropbox, hashlib
 
-# Read Access token from text file and store as a list of object
+#Read input file
+
+
+
+#Create Key by hash input file with sha256
+
+input = 'hello'
+hash_object = hashlib.sha256(b'test')
+hex_dig = hash_object.hexdigest()
+#print(hex_dig)
+
+
+
+# Read Dropbox Access token from text file and store as a list of object
 f = open('../token.txt', 'r')
 token = f.readlines()
-print(token[0])
+#print(token[0])
 
 
 #Connect to Dropbox application with access token
+dbx = dropbox.Dropbox(token[0])
+
+#get dropbox user account
+
+dbx_user = dbx.users_get_current_account()
+print(dbx_user)
