cd4fd02774bef670d8bbba4df58f2c55c7a26d03
Chanchai <chanchai@uab.edu>
2018-04-11 18:16:39 -0500

    Working on share file with Bob

--- A2.py
+++ A2.py
@@ -6,6 +6,7 @@
 # https://gist.github.com/lkdocs/6519359
 # https://stackoverflow.com/questions/606191/convert-bytes-to-a-string
 # https://www.dlitz.net/software/pycrypto/api/current/Crypto.Util.Counter-module.html
+# https://www.datacamp.com/community/tutorials/functions-python-tutorial
 
 import dropbox, hashlib, os,codecs
 from Crypto import Random
@@ -16,150 +17,168 @@ from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
 
 
-#Read input file
-f = open("input.txt","r") #opens file with name of "test.txt"
+def main():
+    #Read input file
 
-input = f.read()
-print("################ Input Content #######################")
-print(input)
+    f = open("input.txt","r") #opens file with name of "test.txt"
 
-#1.Create K by hash input file with sha256
-hash_object = hashlib.sha256(input.encode('utf-8'))
-K = hash_object.hexdigest()
+    input = f.read()
+    print("################ Input Content #######################")
+    print(input)
 
-print("################print K from hashlib.sha256(input) #######################")
-print("K: "+ K)
+    #_______________________________________________________________________________________________
+    #1.Create K by hash input file with sha256
+    hash_object = hashlib.sha256(input.encode('utf-8'))
+    K = hash_object.hexdigest()
 
-#_______________________________________________________________________________________________
+    H = hash_object.hexdigest()
 
+    print("################print K from hashlib.sha256(input) #######################")
+    print("Key from SHA256 encryption: "+ K)
 
-
-#2 Create Cipher text C with AES Counter mode
-
-####################Modified############################
-
-random_generator = Random.new()
-IV = random_generator.read(8)
-#modified
-keye = (K)[:32]
-ctr_e = Counter.new(64, prefix=IV)
-encryptor = AES.new(keye, AES.MODE_CTR, counter=ctr_e)
-ciphertext = encryptor.encrypt(input)
-print("ciphertext:")
-print(ciphertext)
-print("_________________________")
-
-####################Modified############################
-
-#_______________________________________________________________________________________________
-
-
-#3 Encrypt K with the sender's RSA public key
-	# Input K and Sender's public key
-	# Output W
-	# Ref: https://www.laurentluce.com/posts/python-and-cryptography-with-pycrypto/#a_3
-#ba = bitarray.bitarray()
-#key = RSA.generate(1024,ba.frombytes(key1.encode('utf-8')))
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator)
-
-print("Sender's public key")
-print(key.publickey())
-
-print("Sender's private key")
-print(key)
-
-# Note : key = private key, key.publickey() = public key = public_key
-public_key = key.publickey()
-
-
-W = public_key.encrypt(K.encode('utf-8'), 32)
-#W = public_key.encrypt(K, 32)
-
-print("\n\n W is")
-print(W)
-print(type(W))
-
-print(type(W[0]))
-
-
-#_______________________________________________________________________________________________
-#4 Upload Ciphertext (C) and W to dropbox
-# Read Dropbox Access token from text file and store as a list of object
-f = open('../token.txt', 'r')
-#f = open('../sample.txt', 'r')
-token = f.readlines()
-#print(token[0])
-
-
-#Connect to Dropbox application with access token
-dbx = dropbox.Dropbox(token[0])
-
-#get dropbox user account
-dbx_user = dbx.users_get_current_account()
-#print(dbx_user)
-
-#Upload C (CipherText)
-ciphertext_fileName='/A2/C'
-dbx.files_upload(ciphertext, ciphertext_fileName, mode=WriteMode('overwrite'))
-print("Finished Upload C(CipherText). File location: /A2/C");
-
-
-#Upload W(Encrypted Key)
-encrypted_key='/A2/W'
-dbx.files_upload(W[0], encrypted_key, mode=WriteMode('overwrite'))
-print("Finished Upload W(Key Encryption). File location: /A2/W")
-
-#_______________________________________________________________________________________________
-##############################################################################################
-
-#Decryption
-
-#_______________________________________________________________________________________________
-#1. Downloading C and W from cloud storage
-
-####################Download C #############################
-try:
-    md, res = dbx.files_download('/A2/C')
-except dropbox.exceptions.HttpError as err:
-    print('*** HTTP error', err)
-
-
-data_C = res.content
-print ("Downdloaded Ciphertext")
-# print(len(data_C), 'bytes; md:', md)
-print(data_C)
+    #_______________________________________________________________________________________________
 
 
 
-###################Download W ################
+    #2 Create Cipher text C with AES Counter mode
 
-try:
-    md, res = dbx.files_download('/A2/W')
-except dropbox.exceptions.HttpError as err:
-    print('*** HTTP error', err)
+    #IV = Initial Vector for Counter Mode Prefix
+
+    random_generator = Random.new()
+    IV = random_generator.read(8)
+    #modified
+    keye = (K)[:32]
+    ctr_e = Counter.new(64, prefix=IV)
+    encryptor = AES.new(keye, AES.MODE_CTR, counter=ctr_e)
+    ciphertext = encryptor.encrypt(input)
+    print("ciphertext:")
+    print(ciphertext)
+    print("_________________________")
 
 
-data = res.content
-# print(len(data), 'bytes; md:', md)
-print ("Downdloaded W")
-print(data)
-#_______________________________________________________________________________________________
 
-#2. Extract Key K from W by using RSA decryption.
-decrypted_W = key.decrypt(data)
-print("Decrypted Key:")
-print(decrypted_W)
+    #_______________________________________________________________________________________________
 
-#_______________________________________________________________________________________________
-#3.Decypteing the chiphertext C with AES-CTR withd decrypted_W=K
-keyd = (decrypted_W)[:32]
 
-# Create counter for decryptor with IV
-ctr_d = Counter.new(64, prefix=IV)
+    #3 Encrypt K with the sender's RSA public key
+    	# Input K and Sender's public key
+    	# Output W
+    	# Ref: https://www.laurentluce.com/posts/python-and-cryptography-with-pycrypto/#a_3
+    #ba = bitarray.bitarray()
+    #key = RSA.generate(1024,ba.frombytes(key1.encode('utf-8')))
+    random_generator = Random.new().read
+    key = RSA.generate(1024, random_generator)
 
-# Create decryptor, then decrypt and print decoded text
-decryptor = AES.new(keyd, AES.MODE_CTR, counter=ctr_d)
-decoded_text = decryptor.decrypt(data_C)
-print ("Plaintext:\n"+decoded_text.decode('utf-8'))
-#_______________________________________________________________________________________________
+    print("Sender's public key")
+    print(key.publickey())
+
+    print("Sender's private key")
+    print(key)
+
+    # Note : key = private key, key.publickey() = public key = public_key
+    public_key = key.publickey()
+
+
+    W = public_key.encrypt(K.encode('utf-8'), 32)
+    #W = public_key.encrypt(K, 32)
+
+    print("\n\n W is")
+    print(W)
+    print(type(W))
+
+    print(type(W[0]))
+
+
+    #_______________________________________________________________________________________________
+    #4 Upload Ciphertext (C) and W to dropbox
+    # Read Dropbox Access token from text file and store as a list of object
+    f = open('../token.txt', 'r')
+    #f = open('../sample.txt', 'r')
+    token = f.readlines()
+    #print(token[0])
+
+
+    #Connect to Dropbox application with access token
+    dbx = dropbox.Dropbox(token[0])
+
+    #get dropbox user account
+    dbx_user = dbx.users_get_current_account()
+    #print(dbx_user)
+
+    #Upload C (CipherText)
+    ciphertext_fileName='/A2/C'
+    dbx.files_upload(ciphertext, ciphertext_fileName, mode=WriteMode('overwrite'))
+    print("Finished Upload C(CipherText). File location: /A2/C");
+
+
+    #Upload W(Encrypted Key)
+    encrypted_key='/A2/W'
+    dbx.files_upload(W[0], encrypted_key, mode=WriteMode('overwrite'))
+    print("Finished Upload W(Key Encryption). File location: /A2/W")
+
+    #_______________________________________________________________________________________________
+    ##############################################################################################
+
+    #Decryption
+
+    #_______________________________________________________________________________________________
+    #1. Downloading C and W from cloud storage
+
+    ####################Download C #############################
+    try:
+        md, res = dbx.files_download('/A2/C')
+    except dropbox.exceptions.HttpError as err:
+        print('*** HTTP error', err)
+
+
+    data_C = res.content
+    print ("Downdloaded Ciphertext")
+    # print(len(data_C), 'bytes; md:', md)
+    print(data_C)
+    print("Ciphertext URL:")
+    print(dbx.sharing_get_file_metadata('/A2/C').preview_url)
+
+
+    ###################Download W ################
+
+    try:
+        md, res = dbx.files_download('/A2/W')
+    except dropbox.exceptions.HttpError as err:
+        print('*** HTTP error', err)
+
+
+    data = res.content
+    # print(len(data), 'bytes; md:', md)
+    print ("Downdloaded W")
+    print(data)
+    #_______________________________________________________________________________________________
+
+    #2. Extract Key K from W by using RSA decryption.
+    decrypted_W = key.decrypt(data)
+    print("Decrypted Key:")
+    print(decrypted_W)
+
+    #_______________________________________________________________________________________________
+    #3.Decypteing the chiphertext C with AES-CTR withd decrypted_W=K
+    keyd = (decrypted_W)[:32]
+
+    # Create counter for decryptor with IV
+    ctr_d = Counter.new(64, prefix=IV)
+
+    # Create decryptor, then decrypt and print decoded text
+    decryptor = AES.new(keyd, AES.MODE_CTR, counter=ctr_d)
+    decoded_text = decryptor.decrypt(data_C)
+    print ("Plaintext:\n"+decoded_text.decode('utf-8'))
+    #_______________________________________________________________________________________________
+
+    ###############################################################################################
+    # Secure File Sharing
+
+    #1. Fetch W from URL
+    #URL: https://www.dropbox.com/s/ropcnvj1x3o7ux6/W?dl=0
+    # url = 'https://www.dropbox.com/s/ropcnvj1x3o7ux6/W?dl=0'
+    # d = wget.download(url)
+    # print(d)
+
+if __name__ == '__main__':
+    main()
